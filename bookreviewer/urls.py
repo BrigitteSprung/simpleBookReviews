7e92f32a058711a545b5f176eec4c60de6bb3a48
BrigitteSprung <brigittesprung@gmail.com>
2021-07-03 18:09:38 +0200

    Added crud, just need to do the form for the update

--- bookreviewer/urls.py
+++ bookreviewer/urls.py
@@ -5,5 +5,10 @@ from . import views
 app_name = 'bookreviewer'
 
 urlpatterns = [
-    path('', views.review_list, name='review_list'),
+    path('', views.review_list, name='list'),
+    path('create/', views.review_create, name='create'),
+    path('<id>', views.review_detail, name='detail'),
+    path('<id>/update/', views.review_update, name='update'),
+    path('<id>/delete/', views.review_delete, name='delete'),
+
 ]
--- bookreviewer/views.py
+++ bookreviewer/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Review
 
@@ -12,3 +12,28 @@ def review_list(request):
 
 
 # Need to set up a CRUD
+
+# Retrieve
+def review_detail(request, id):
+    review = Review.objects.get(id=id)
+    context = {
+        "review": review
+    }
+    return render(request, "review_detail.html", context)
+
+def review_create(request):
+    return HttpResponse("Create a review")
+
+
+def review_update(request, id):
+    review = Review.objects.get(id=id)
+    context = {
+        "review": review
+    }
+    return render(request, "review_update.html", context)
+
+
+def review_delete(request, id):
+    review = Review.objects.get(id=id)
+    review.delete()
+    return redirect('/')
