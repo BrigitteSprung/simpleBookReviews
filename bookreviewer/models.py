23328e0adda8ea1853b8cd10fe9eb3255599fa0a
BrigitteSprung <brigittesprung@gmail.com>
2021-07-03 17:47:51 +0200

    Some simple functions. Got a view with review lists now.

--- bookreviewer/models.py
+++ bookreviewer/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+class Review(models.Model):
+    book_name = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    date_completed = models.DateField(default=timezone.now())
+    rating = models.IntegerField(default=0)
+    review = models.TextField()
+
+    def __str__(self):
+        return self.book_name
--- bookreviewer/views.py
+++ bookreviewer/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Review
 
 # Create your views here.
+def review_list(request):
+    reviews = Review.objects.all()
+    context = {
+        "reviews": reviews,
+    }
+    return render(request, "review_list.html", context)
+
+
+# Need to set up a CRUD
